8a26b5c037c6ab0fc903efe7db63a27c76b329ee
Michael Hallam <hallam.michael.a@gmail.com>
2024-09-19 15:30:49 +0100

    Added ability to load presets from json files

--- BallSimulation/Main.cpp
+++ BallSimulation/Main.cpp
@@ -1,59 +1,30 @@
 /*
  ********* TORUS PARTICLE SIMULATOR *********
  *
- * This program simulates particles colliding on a torus
+ * A simple program to simulate particles colliding on a torus
  * 
  * Support is available for particles of different masses and radii
  * 
  * 
  */
 
-
-
 #include <iostream>
 
-#include "vec2.hpp"
 #include "balltype.hpp"
 #include "Renderer.hpp"
 #include "Solver.hpp"
 #include "BruteForceSolver.hpp"
 #include "PruneAndSweep1DSolver.hpp"
+#include "loadPreset.hpp"
 
 int main()
 {
 	// Set simulation parameters
-
 	float dt = 0.002f;
-
-    balltype b1;
-    b1.radius = 0.15f;
-    b1.mass = 2.4f;
-    b1.count = 1;
-    b1.rgba = { 1.0f, 0.5f, 0.2f, 1.0f };
-    b1.totalMomentum = { 0.0f, 0.0f };
-    b1.wrapTexture = true;
-    b1.render = true;
-
-    balltype b2;
-    b2.radius = 0.04f;
-    b2.mass = 0.9f;
-    b2.count = 40;
-    b2.rgba = { 0.0f, 0.5f, 0.8f, 1.0f };
-    b2.totalMomentum = { 0.0f, 0.0f };
-    b2.wrapTexture = true;
-    b2.render = true;
-
-    balltype b3;
-    b3.radius = 0.005f;
-    b3.mass = 0.1f;
-    b3.count = 4000;
-    b3.rgba = { 0.7f, 0.1f, 0.6f, 1.0f };
-    b3.totalMomentum = { 0.0f, 0.0f };
-    b3.wrapTexture = false;
-    b3.render = true;
+    std::vector<balltype> preset = loadPreset("../res/preset1.json");
 
 	// Initialise simulation
-    PruneAndSweep1DSolver solver({b1, b2, b3});
+    PruneAndSweep1DSolver solver(preset);
 
     // Initialise renderer
     unsigned int resolution = 860;
